2d1e4e3c2a2fc4ee8c0d98e71ee60b907dca7c0c
pgurung-takeo <Prakhyat@takeo.ai>
2025-09-08 02:00:47 -0400

    added a scratch file about inheritance

--- app/src/main/java/com/welovecrazyquote/psandroidprojects/MainActivity.java
+++ app/src/main/java/com/welovecrazyquote/psandroidprojects/MainActivity.java
@@ -15,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Project gettingStartedProject = new Project("Getting Started App", "Our very first project, the default 'Hello World' app!", 5);
+        Project[] projects = {
+                new Project("Getting Started App", "Our very first project that displays the classic 'Hello World' to learn the basics of Android development.", R.drawable.getting_started),
+                new Project("Crazy Quote", "An app that shows random motivational and fun quotes every time you open it.", R.drawable.quote),
+                new Project("BMI Calculator", "A simple health tool to calculate Body Mass Index based on height and weight.", R.drawable.calculator),
+                new Project("Inches Converter", "A handy converter app that lets users quickly switch between inches, centimeters, and other units.", R.drawable.tape),
+                new Project("The Hungry Minds", "A restaurant-style demo app featuring menus, categories, and navigation with RecyclerView and CardView.", R.drawable.hungry_developer),
+
+        };
     }
 }
