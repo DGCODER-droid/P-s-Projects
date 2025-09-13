83aa160b2b0dc568123377e1ff5ea77b89b96275
pgurung-takeo <Prakhyat@takeo.ai>
2025-09-13 19:02:54 -0400

    Displayed the list of projects using recycler view

--- app/src/main/java/com/welovecrazyquote/psandroidprojects/MainActivity.java
+++ app/src/main/java/com/welovecrazyquote/psandroidprojects/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Project gettingStartedProject = new Project("Getting Started App", "Our very first project, the default 'Hello World' app!", 5);
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
         Project[] projects = {
                 new Project("Getting Started App", "Our very first project that displays the classic 'Hello World' to learn the basics of Android development.", R.drawable.getting_started),
                 new Project("Crazy Quote", "An app that shows random motivational and fun quotes every time you open it.", R.drawable.quote),
@@ -24,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 new Project("The Hungry Minds", "A restaurant-style demo app featuring menus, categories, and navigation with RecyclerView and CardView.", R.drawable.hungry_developer),
 
         };
+        ProjectAdapter adapter = new ProjectAdapter(projects);
+        list.setAdapter(adapter);
     }
 }
